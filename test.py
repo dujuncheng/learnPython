7812cd23b737c756573bf4add06a876c42f176b8
dujuncheng <1390851423@qq.com>
2017-12-26 22:58:16 +0800

    test

--- test.py
+++ test.py
@@ -10,4 +10,39 @@ print bicycles;
 bicycles[1] = 'dudu'
 print bicycles;
 
-bicycles
+# 列表最后一项添加元素
+bicycles.append(1)
+print bicycles;
+
+# 列表最后一项删除，并且返回删除的项
+num =  bicycles.pop();
+print bicycles;
+print 'delete num ' + str(num);
+
+# 列表中插入，第一个参数表示插入的位置，第二个是插入的东西
+bicycles.insert(2,'ssss');
+print bicycles;
+
+# 随机删除
+del bicycles[2];
+print bicycles;
+
+# pop(index) del 如何取舍？ 删除的东西有用，用pop(); 没有用， 用del
+
+arr = ['a','b','c'];
+for item in arr:
+    print item;
+print 'done'
+
+
+#  range(1,5)
+nums = range(1,6);
+print nums;
+
+
+#
+square = [];
+for item in range(1,11):
+    square.append(item**item);
+
+print square;
